aa44af0c4fc0b8a8bfd8ba9bfaccaee0dab5023d
remiie <virusloki@ya.ru>
2023-04-29 20:54:55 +0300

    add handle tap func, update light, create cube

--- ARMix/ViewController.swift
+++ ARMix/ViewController.swift
@@ -9,19 +9,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(sceneView)
+        
         NSLayoutConstraint.activate([
             sceneView.topAnchor.constraint(equalTo: view.topAnchor),
             sceneView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             sceneView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             sceneView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
+        
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal, .vertical]
         sceneView.session.run(configuration)
         sceneView.delegate = self
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        sceneView.addGestureRecognizer(tapGesture)
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -34,9 +39,36 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.viewWillDisappear(animated)
         sceneView.session.pause()
     }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let lightEstimation = sceneView.session.currentFrame?.lightEstimate else { return }
+        
+        let ambientLight = SCNLight()
+        ambientLight.type = .ambient
+        ambientLight.intensity = lightEstimation.ambientIntensity
+        ambientLight.temperature = lightEstimation.ambientColorTemperature
+    
+        sceneView.scene.rootNode.light = ambientLight
+        sceneView.scene.rootNode.categoryBitMask = 1
+    }
 
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        // code to add 3D models to the scene
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        let location = sender.location(in: sceneView)
+        let results = sceneView.hitTest(location, types: [.existingPlaneUsingExtent, .estimatedHorizontalPlane])
+        if let hitTestResult = results.first {
+            let position = hitTestResult.worldTransform.columns.3
+            let cubeNode = createCubeNode()
+            cubeNode.position = SCNVector3(position.x, position.y, position.z)
+            sceneView.scene.rootNode.addChildNode(cubeNode)
+        }
+    }
+    
+    
+    func createCubeNode() -> SCNNode {
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        let cubeNode = SCNNode(geometry: cube)
+        cubeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        return cubeNode
     }
 }
 
